3a14353688f796c14129977afbf6815ba709f724
213033aumc <105603840+213033aumc@users.noreply.github.com>
2023-10-17 14:31:07 +0500

    Add files via upload

---
+++ 213033_Unit Converter/Unit Converter/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Unit_Converter
+{
+    partial class UnitConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltype = new System.Windows.Forms.Label();
+            this.cbtype = new System.Windows.Forms.ComboBox();
+            this.lblfrom = new System.Windows.Forms.Label();
+            this.tbfrom = new System.Windows.Forms.TextBox();
+            this.cbfrom = new System.Windows.Forms.ComboBox();
+            this.lblto = new System.Windows.Forms.Label();
+            this.tbto = new System.Windows.Forms.TextBox();
+            this.cbto = new System.Windows.Forms.ComboBox();
+            this.btnconvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltype
+            // 
+            this.lbltype.AutoSize = true;
+            this.lbltype.Location = new System.Drawing.Point(447, 33);
+            this.lbltype.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbltype.Name = "lbltype";
+            this.lbltype.Size = new System.Drawing.Size(62, 13);
+            this.lbltype.TabIndex = 0;
+            this.lbltype.Text = "Select Unit ";
+            // 
+            // cbtype
+            // 
+            this.cbtype.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbtype.FormattingEnabled = true;
+            this.cbtype.Items.AddRange(new object[] {
+            "Length",
+            "Temperature",
+            "Area",
+            "Time"});
+            this.cbtype.Location = new System.Drawing.Point(334, 60);
+            this.cbtype.Margin = new System.Windows.Forms.Padding(2);
+            this.cbtype.Name = "cbtype";
+            this.cbtype.Size = new System.Drawing.Size(320, 21);
+            this.cbtype.TabIndex = 1;
+            this.cbtype.SelectedIndexChanged += new System.EventHandler(this.cbtype_SelectedIndexChanged);
+            // 
+            // lblfrom
+            // 
+            this.lblfrom.AutoSize = true;
+            this.lblfrom.Location = new System.Drawing.Point(306, 102);
+            this.lblfrom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblfrom.Name = "lblfrom";
+            this.lblfrom.Size = new System.Drawing.Size(30, 13);
+            this.lblfrom.TabIndex = 2;
+            this.lblfrom.Text = "From";
+            // 
+            // tbfrom
+            // 
+            this.tbfrom.Location = new System.Drawing.Point(308, 118);
+            this.tbfrom.Margin = new System.Windows.Forms.Padding(2);
+            this.tbfrom.Name = "tbfrom";
+            this.tbfrom.Size = new System.Drawing.Size(141, 20);
+            this.tbfrom.TabIndex = 3;
+            this.tbfrom.TextChanged += new System.EventHandler(this.tbfrom_TextChanged);
+            // 
+            // cbfrom
+            // 
+            this.cbfrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbfrom.FormattingEnabled = true;
+            this.cbfrom.Location = new System.Drawing.Point(308, 142);
+            this.cbfrom.Margin = new System.Windows.Forms.Padding(2);
+            this.cbfrom.Name = "cbfrom";
+            this.cbfrom.Size = new System.Drawing.Size(141, 21);
+            this.cbfrom.TabIndex = 4;
+            // 
+            // lblto
+            // 
+            this.lblto.AutoSize = true;
+            this.lblto.Location = new System.Drawing.Point(538, 102);
+            this.lblto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblto.Name = "lblto";
+            this.lblto.Size = new System.Drawing.Size(20, 13);
+            this.lblto.TabIndex = 5;
+            this.lblto.Text = "To";
+            // 
+            // tbto
+            // 
+            this.tbto.Enabled = false;
+            this.tbto.Location = new System.Drawing.Point(540, 118);
+            this.tbto.Margin = new System.Windows.Forms.Padding(2);
+            this.tbto.Name = "tbto";
+            this.tbto.Size = new System.Drawing.Size(141, 20);
+            this.tbto.TabIndex = 6;
+            this.tbto.TextChanged += new System.EventHandler(this.tbto_TextChanged);
+            // 
+            // cbto
+            // 
+            this.cbto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbto.FormattingEnabled = true;
+            this.cbto.Location = new System.Drawing.Point(541, 142);
+            this.cbto.Margin = new System.Windows.Forms.Padding(2);
+            this.cbto.Name = "cbto";
+            this.cbto.Size = new System.Drawing.Size(140, 21);
+            this.cbto.TabIndex = 7;
+            // 
+            // btnconvert
+            // 
+            this.btnconvert.Location = new System.Drawing.Point(471, 202);
+            this.btnconvert.Margin = new System.Windows.Forms.Padding(2);
+            this.btnconvert.Name = "btnconvert";
+            this.btnconvert.Size = new System.Drawing.Size(56, 19);
+            this.btnconvert.TabIndex = 8;
+            this.btnconvert.Text = "Convert";
+            this.btnconvert.UseVisualStyleBackColor = true;
+            this.btnconvert.Click += new System.EventHandler(this.btnconvert_Click);
+            // 
+            // UnitConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 562);
+            this.Controls.Add(this.btnconvert);
+            this.Controls.Add(this.cbto);
+            this.Controls.Add(this.tbto);
+            this.Controls.Add(this.lblto);
+            this.Controls.Add(this.cbfrom);
+            this.Controls.Add(this.tbfrom);
+            this.Controls.Add(this.lblfrom);
+            this.Controls.Add(this.cbtype);
+            this.Controls.Add(this.lbltype);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "UnitConverter";
+            this.Text = "Unit Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltype;
+        private System.Windows.Forms.ComboBox cbtype;
+        private System.Windows.Forms.Label lblfrom;
+        private System.Windows.Forms.TextBox tbfrom;
+        private System.Windows.Forms.ComboBox cbfrom;
+        private System.Windows.Forms.Label lblto;
+        private System.Windows.Forms.TextBox tbto;
+        private System.Windows.Forms.ComboBox cbto;
+        private System.Windows.Forms.Button btnconvert;
+    }
+}
+
---
+++ 213033_Unit Converter/Unit Converter/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Unit_Converter
+{
+    public partial class UnitConverter : Form
+    {
+
+        public UnitConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbfrom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbtype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            if (cbtype.SelectedItem == "Length")
+            {
+                cbfrom.Items.Clear();
+                cbto.Items.Clear();
+                cbfrom.Items.Add("Centimetres");
+                cbfrom.Items.Add("Metres");
+                cbfrom.Items.Add("Kilometres");
+                cbto.Items.Add("Centimetres");
+                cbto.Items.Add("Metres");
+                cbto.Items.Add("Kilometres");
+            }
+
+            if (cbtype.SelectedItem == "Temperature")
+            {
+                cbfrom.Items.Clear();
+                cbto.Items.Clear();
+                cbfrom.Items.Add("Celsius");
+                cbfrom.Items.Add("Fahrenheit");
+                cbfrom.Items.Add("Kelvin");
+                cbto.Items.Add("Celsius");
+                cbto.Items.Add("Fahrenheit");
+                cbto.Items.Add("Kelvin");
+            }
+            if (cbtype.SelectedItem == "Area")
+            {
+                cbfrom.Items.Clear();
+                cbto.Items.Clear();
+
+                cbfrom.Items.Add("Metre");
+                cbfrom.Items.Add("Inches");
+                cbfrom.Items.Add("Foot");
+                cbto.Items.Add("Metre");
+                cbto.Items.Add("Inches");
+                cbto.Items.Add("Foot");
+
+            }
+            if (cbtype.SelectedItem == "Time")
+            {
+                cbfrom.Items.Clear();
+                cbto.Items.Clear();
+
+                cbfrom.Items.Add("Seconds");
+                cbfrom.Items.Add("Minutes");
+                cbfrom.Items.Add("Hours");
+                cbto.Items.Add("Seconds");
+                cbto.Items.Add("Minutes");
+                cbto.Items.Add("Hours");
+
+            }
+
+        }
+        private void tbto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnconvert_Click(object sender, EventArgs e)
+        {
+            if (tbfrom.Text != "")
+            {
+
+            }
+            if (cbtype.SelectedItem == "Length")
+            {
+
+                if (cbfrom.SelectedItem == "Centimetres" && cbto.SelectedItem == "Centimetres")
+                {
+                    tbto.Text = tbfrom.Text;
+                }
+                if (cbfrom.SelectedItem == "Centimetres" && cbto.SelectedItem == "Metres")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) / 100);
+                }
+                if (cbfrom.SelectedItem == "Centimetres" && cbto.SelectedItem == "Kilometres")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) / 100000);
+                }
+
+                if (cbfrom.SelectedItem == "Metres" && cbto.SelectedItem == "Centimetres")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) * 100);
+                }
+                if (cbfrom.SelectedItem == "Metres" && cbto.SelectedItem == "Metres")
+                {
+                    tbto.Text = tbfrom.Text;
+                }
+                if (cbfrom.SelectedItem == "Metres" && cbto.SelectedItem == "Kilometres")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) / 1000);
+                }
+
+            }
+            if (cbfrom.SelectedItem == "Kilometres" && cbto.SelectedItem == "Centimetres")
+            {
+                tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) * 100000);
+            }
+            if (cbfrom.SelectedItem == "Kilometres" && cbto.SelectedItem == "Metres")
+            {
+                tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) * 1000);
+            }
+            if (cbfrom.SelectedItem == "Kilometres" && cbto.SelectedItem == "Kilometres")
+            {
+                tbto.Text = tbfrom.Text;
+            }
+
+
+
+            if (cbtype.SelectedItem == "Temperature")
+            {
+                if (cbfrom.SelectedItem == "Celsius" && cbto.SelectedItem == "Fahrenheit")
+                {
+                    Double C = Convert.ToDouble(tbfrom.Text);
+                    tbto.Text = Convert.ToString((1.8 * C) + 32);
+                }
+                if (cbfrom.SelectedItem == "Fahrenheit" && cbto.SelectedItem == "Celsius")
+                {
+                    Double F = Convert.ToDouble(tbfrom.Text);
+                    Double C = (F - 32) * (5.0 / 9.0);
+                    tbto.Text = Convert.ToString(C);
+                }
+                if (cbfrom.SelectedItem == "Fahrenheit" && cbto.SelectedItem == "Kelvin")
+                {
+                    Double F = Convert.ToDouble(tbfrom.Text);
+                    Double K = (F - 32) * (5.0 / 9.0) + 273.15;
+                    tbto.Text = Convert.ToString(K);
+                }
+                if (cbfrom.SelectedItem == "Kelvin" && cbto.SelectedItem == "Fahrenheit")
+                {
+                    Double K = Convert.ToDouble(tbfrom.Text);
+                    Double F = (K - 273.15) * (9.0 / 5.0) + 32;
+                    tbto.Text = Convert.ToString(F);
+                }
+                if (cbfrom.SelectedItem == "Celsius" && cbto.SelectedItem == "Kelvin")
+                {
+                    Double C = Convert.ToDouble(tbfrom.Text);
+                    Double K = C + 273.15;
+                    tbto.Text = Convert.ToString(K);
+                }
+                if (cbfrom.SelectedItem == "Kelvin" && cbto.SelectedItem == "Celsius")
+                {
+                    Double K = Convert.ToDouble(tbfrom.Text);
+                    Double C = K - 273.15;
+                    tbto.Text = Convert.ToString(C);
+                }
+                if (cbfrom.SelectedItem == "Celsius" && cbto.SelectedItem == "Celsius")
+                {
+                    tbto.Text = tbfrom.Text;
+                }
+                if (cbfrom.SelectedItem == "Fahrenheit" && cbto.SelectedItem == "Fahrenheit")
+                {
+                    tbto.Text = tbfrom.Text;
+                }
+                if (cbfrom.SelectedItem == "Kelvin" && cbto.SelectedItem == "Kelvin")
+                {
+                    tbto.Text = tbfrom.Text;
+                }
+            }
+
+            if (cbtype.SelectedItem == "Area")
+            {
+
+
+                if (cbfrom.SelectedItem == "Metre" && cbto.SelectedItem == "Metre")
+                {
+                    tbto.Text = tbfrom.Text;
+                }
+
+                if (cbfrom.SelectedItem == "Metre" && cbto.SelectedItem == "Inches")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) * 1550.003);
+                }
+                if (cbfrom.SelectedItem == "Metre" && cbto.SelectedItem == "Foot")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) * 10.76391);
+                }
+
+                if (cbfrom.SelectedItem == "Inches" && cbto.SelectedItem == "Metre")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) / 1550.003);
+                }
+
+                if (cbfrom.SelectedItem == "Inches" && cbto.SelectedItem == "Inches")
+                {
+                    tbto.Text = tbfrom.Text;
+                }
+                if (cbfrom.SelectedItem == "Inches" && cbto.SelectedItem == "Foot")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) / 144);
+                }
+
+
+                if (cbfrom.SelectedItem == "Foot" && cbto.SelectedItem == "Metre")
+                {
+                    tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) / 3.281);
+
+                    if (cbfrom.SelectedItem == "Foot" && cbto.SelectedItem == "Inches")
+                    {
+                        tbto.Text = Convert.ToString(Convert.ToDouble(tbfrom.Text) * 144);
+                    }
+                    if (cbfrom.SelectedItem == "Foot" && cbto.SelectedItem == "Foot")
+                    {
+                        tbto.Text = tbfrom.Text;
+                    }
+
+
+
+
+                    if (cbtype.SelectedItem == "Time")
+                    {
+
+                        if (cbfrom.SelectedItem == "Seconds" && cbto.SelectedItem == "Seconds")
+                        {
+                            tbto.Text = tbfrom.Text;
+                        }
+                        if (cbfrom.SelectedItem == "Seconds" && cbto.SelectedItem == "Minutes")
+                        {
+                            tbto.Text = Convert.ToString(Decimal.Parse(tbfrom.Text) / 60);
+                        }
+                        if (cbfrom.SelectedItem == "Seconds" && cbto.SelectedItem == "Hours")
+                        {
+                            tbto.Text = Convert.ToString(Decimal.Parse(tbfrom.Text) / 3600);
+                        }
+
+
+                        if (cbfrom.SelectedItem == "Minutes" && cbto.SelectedItem == "Seconds")
+                        {
+                            tbto.Text = Convert.ToString(Decimal.Parse(tbfrom.Text) * 60);
+                        }
+                        if (cbfrom.SelectedItem == "Minutes" && cbto.SelectedItem == "Minutes")
+                        {
+                            tbto.Text = tbfrom.Text;
+                        }
+                        if (cbfrom.SelectedItem == "Minutes" && cbto.SelectedItem == "Hours")
+                        {
+                            tbto.Text = Convert.ToString(Decimal.Parse(tbfrom.Text) / 60);
+                        }
+
+
+                        if (cbfrom.SelectedItem == "Hours" && cbto.SelectedItem == "Seconds")
+                        {
+                            tbto.Text = Convert.ToString(Decimal.Parse(tbfrom.Text) * 3600);
+                        }
+                        if (cbfrom.SelectedItem == "Hours" && cbto.SelectedItem == "Minutes")
+                        {
+                            tbto.Text = Convert.ToString(Decimal.Parse(tbfrom.Text) * 60);
+                        }
+                        if (cbfrom.SelectedItem == "Hours" && cbto.SelectedItem == "Hours")
+                        {
+                            tbto.Text = tbfrom.Text;
+                        }
+
+                    }
+                }
+
+                else
+                {
+                    tbto.Text = "";
+                }
+
+            }
+        }
+    }
+}
