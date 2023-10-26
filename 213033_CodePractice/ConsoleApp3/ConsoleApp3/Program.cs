d9a629064059a14473331bf842cef49c16366f01
213033aumc <105603840+213033aumc@users.noreply.github.com>
2023-10-26 20:59:47 +0500

    Add files via upload

---
+++ 213033_CodePractice/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ali Shaan - 213033
+using System;
+namespace c {
+    class pro { 
+        public static void Main(String[] args) 
+        {
+            int n,i = 0;
+            Console.WriteLine("Enter value: ");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            { 
+              i++;
+              n = n / 10;
+            }
+            Console.WriteLine("Magnitude of given number is : "+i);
+        }
+    } 
+}
