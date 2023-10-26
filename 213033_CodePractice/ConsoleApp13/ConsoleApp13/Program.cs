7b6f8c271afefc49622c82c731dddb4c60ebc275
213033aumc <105603840+213033aumc@users.noreply.github.com>
2023-10-26 21:12:19 +0500

    Add files via upload

---
+++ 213033_CodePractice/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Ali Shaan - 213033
+using System;
+    class Enumtype 
+{ 
+    enum Direction 
+    { 
+        North,
+        East = 10,
+        West, 
+        South 
+    } 
+    public static void Main()
+    {
+        Direction d1 = 0;
+        Direction d2 = Direction.East;
+        Direction d3 = Direction.West;
+        Direction d4 = (Direction)12;
+        Console.WriteLine("d1= " + d1);
+        Console.WriteLine("d2= " + (int)d2);
+        Console.WriteLine("d3= " + d3);
+        Console.WriteLine("d4= " + d4);
+    }
+}
