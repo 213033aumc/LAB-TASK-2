14885eab4aef08d5a0405e6f1c04c59b7909eced
213033aumc <105603840+213033aumc@users.noreply.github.com>
2023-10-26 20:53:13 +0500

    Add files via upload

---
+++ 213033_CodePractice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Ali Shaan - 213033
+using System;
+namespace Factor { 
+    class Program { 
+        public static void Main() 
+        {
+            int i = 2, n;
+            Console.WriteLine("Enter n value: ");
+            n = int.Parse(Console.ReadLine());
+            while (i <= n) 
+            { 
+                if (n < 10 && i <= n)
+                {
+                    Console.WriteLine(n + " doen't have single digit factor other than one.");
+                    break;
+                } 
+                if (n % i == 0) 
+                {
+                    Console.WriteLine(i + " is smaller factor.");
+                    break;
+                } 
+                i++; 
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ 213033_CodePractice/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Ali Shaan - 213033
+using System;
+namespace prime { 
+    class Pro 
+    {
+        public static void Main()
+        {
+            int i, c, m = 1;
+            Console.WriteLine("Enter a value: ");
+
+            int n = int.Parse(Console.ReadLine());
+            c = 0;
+            i = 1;
+            while (i <= n)
+            {
+                if (n % i == 0)
+                {
+                    c++;
+                    if (i != n);
+                    {
+                        m = i;
+                    }
+                    
+                }
+             i++;
+            }
+            if (c == 2)
+            {
+                Console.WriteLine("It is Prime.");
+            }
+            else
+            { 
+            Console.WriteLine(m + " is the largest factor for " + n);
+            }
+
+            Console.ReadKey();
+        }
+    } 
+}
